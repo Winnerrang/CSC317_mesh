8e2cd1485bd9b5a6a15b73e75761e45466b0bc17
Winnerrang <leo.hanxu@mail.utoronto.ca>
2022-10-31 22:42:48 -0400

    Finsih write_obj.cpp but has not tested it

--- src/write_obj.cpp
+++ src/write_obj.cpp
@@ -3,6 +3,8 @@
 #include <cassert>
 #include <iostream>
 
+// asume input matrix starts from 0
+// therefore we need to add 1 to it for each indices
 bool write_obj(
   const std::string & filename,
   const Eigen::MatrixXd & V,
@@ -15,6 +17,54 @@ bool write_obj(
   assert((F.size() == 0 || F.cols() == 3 || F.cols() == 4) && "F must have 3 or 4 columns");
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here:
+  
+  std::ofstream ofs(filename, std::ios_base::out);
+
+  // Vertex
+  
+  for (int row = 0; row < V.rows(); row++) {
+	  ofs << "v";
+	  for (int col = 0; col < V.cols(); col++) {
+		  ofs << " " << V(row, col);
+	  }
+
+	  ofs << std::endl;
+  }
+  
+  // texture coordinates
+  for (int row = 0; row < UV.rows(); row++) {
+	  ofs << "vt";
+	  for (int col = 0; col < UV.cols(); col++) {
+		  ofs << " " << UV(row, col);
+	  }
+
+	  ofs << std::endl;
+  }
+
+  // normal vectors
+  for (int row = 0; row < NV.rows(); row++) {
+	  ofs << "vn";
+	  for (int col = 0; col < NV.cols(); col++) {
+		  ofs << " " << NV(row, col);
+	  }
+
+	  ofs << std::endl;
+  }
+
+  // faces
+  assert(F.rows() == UF.rows() && F.rows() == NF.rows() && "Number of faces are not equal in three matrices\n");
+  assert(F.cols() == UF.cols() && F.cols() == NF.cols() && "Number of cols are not equal in three matrices\n");
+
+  for (int row = 0; row < NV.rows(); row++) {
+	  ofs << "f";
+	  for (int col = 0; col < NV.cols(); col++) {
+
+		  //matrix index starts from 0, but .obj file's index start from 1
+		  ofs << " " << NF(row, col) + 1 << "/" << UF(row, col) + 1 << "/" << NF(row, col) + 1;
+	  }
+
+	  ofs << std::endl;
+  }
   ////////////////////////////////////////////////////////////////////////////
-  return false;
+  return true;
 }
